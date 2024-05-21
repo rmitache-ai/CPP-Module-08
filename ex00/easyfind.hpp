274c3645c1fc51d1dcc37b7a63a6e03e88424886
Radosalovici-Ionut Mitache <rmitache@student.42heilbronn.de>
2024-05-21 08:26:20 +0200

    Added ex0, not done yet

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+template<typename T>
+/**
+ * @brief Searches for the first occurrence of a given value in a container.
+ * 
+ * This function searches for the first occurrence of the given value in the container specified by the first parameter.
+ * It returns an iterator pointing to the found element if it exists, otherwise it returns an iterator pointing to the end of the container.
+ * 
+ * @tparam T The type of the container.
+ * @param first The container to search in.
+ * @param second The value to search for.
+ * @return An iterator pointing to the found element, or an iterator pointing to the end of the container if the value is not found.
+ */
+T easyfind(T first, int second){
+
+}
