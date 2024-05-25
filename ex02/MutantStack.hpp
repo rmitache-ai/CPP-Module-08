38de1a17c19a63edfdf1eeb89405c0ce9a359629
rmitache <rmitache@student.42heilbronn.de>
2024-05-25 09:08:38 +0000

    refactor: Update MutantStack class with iterator support
    The MutantStack class has been refactored to include iterator support. This allows for easier iteration over the elements in the stack.

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -1,18 +1,35 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
+#include <iostream>
 #include <stack>
-#include <vector>
 
 template <typename T> class MutantStack : public std::stack<T> {
 public:
-	MutantStack();
-	MutantStack(MutantStack const& src);
-	~MutantStack();
+	typedef typename std::stack<T>::container_type::iterator
+		iterator;
 
-	MutantStack& operator=(MutantStack const& rhs);
+	MutantStack()
+		: std::stack<T>() {
+		std::cout << "Constructor called" << std::endl;
+	}
+	MutantStack(MutantStack const& src)
+		: std::stack<T>(src) {
+		std::cout << "Copy Constructor called" << std::endl;
+	}
+	~MutantStack() {
+		std::cout << "Destructor called" << std::endl;
+	};
 
-	std::vector<int> iterators();
+	MutantStack& operator=(MutantStack const& rhs) {
+		if (*this != rhs) {
+			*this = rhs;
+		}
+		return *this;
+	};
+
+	iterator begin() { return (std::stack<T>::c.begin()); }
+	iterator end() { return (std::stack<T>::c.end()); }
 };
 
 #endif /* ***************************************************** MUTANTSTACK_H */
